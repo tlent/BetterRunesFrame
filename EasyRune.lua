e71f5c38733c46067b79d64bfcdcb6912ad77e62
Thomas Lent <tcl293@gmail.com>
2024-01-19 12:20:26 -0500

    Change rune frame appearance

--- EasyRune.lua
+++ EasyRune.lua
@@ -1,11 +1,14 @@
+RUNE_BUTTON_HEIGHT = 40;
+RUNE_HEADER_BUTTON_HEIGHT = 23;
+
 function RuneButtonOnClick(mouseButton, abilityID, equipmentSlot)
-    C_Engraving.CastRune(abilityID)
+    C_Engraving.CastRune(abilityID);
     if mouseButton == "RightButton" then
-        UseInventoryItem(equipmentSlot)
+        UseInventoryItem(equipmentSlot);
         for i = 1, STATICPOPUP_NUMDIALOGS do
-            local popupFrame = _G["StaticPopup" .. i]
+            local popupFrame = _G["StaticPopup" .. i];
             if popupFrame.which == "REPLACE_ENCHANT" and popupFrame:IsVisible() then
-                popupFrame.button1:Click()
+                popupFrame.button1:Click();
                 break
             end
         end
@@ -13,43 +16,104 @@ function RuneButtonOnClick(mouseButton, abilityID, equipmentSlot)
 end
 
 function UpdateButtons()
-    local rune_by_id = {}
-    local categories = C_Engraving.GetRuneCategories(false, false)
+    local numHeaders = 0;
+    local numRunes = 0;
+    local scrollFrame = EngravingFrame.scrollFrame;
+    local buttons = scrollFrame.buttons;
+    local offset = HybridScrollFrame_GetOffset(scrollFrame);
+    local currentOffset = 0;
+    local currentHeader = 1;
+    EngravingFrame_HideAllHeaders();
+    local currentButton = 1;
+    local categories = C_Engraving.GetRuneCategories(true, true);
+    numHeaders = #categories;
+    local rune_by_id = {};
+    local prevRowStart = 1;
     for _, category in ipairs(categories) do
-        local runes = C_Engraving.GetRunesForCategory(category, true)
-        for _, rune in ipairs(runes) do
-            rune_by_id[rune.skillLineAbilityID] = rune
+        if currentOffset < offset then
+            currentOffset = currentOffset + 1;
+        else
+            local button = buttons[currentButton];
+            if button then
+                header = _G["EngravingFrameHeader"..currentHeader];
+				if header then
+                    if prevRowStart > 1 then
+                        button:ClearAllPoints();
+                        button:SetPoint("TOPLEFT", buttons[prevRowStart], "BOTTOMLEFT");
+                    end
+                    prevRowStart = currentButton;
+                    header:ClearAllPoints();
+					header:SetPoint("BOTTOMLEFT", button, 0 , 0);
+                    header:Show();
+					currentHeader = currentHeader + 1;
+					currentButton = currentButton + 1;
+				end
+            end
+        end
+        local runes = C_Engraving.GetRunesForCategory(category, true);
+        numRunes = numRunes + #runes;
+        for runeIndex, rune in ipairs(runes) do
+            rune_by_id[rune.skillLineAbilityID] = rune;
+            if currentOffset < offset then
+				currentOffset = currentOffset + 1;
+			else
+				local button = buttons[currentButton];
+				if button then
+                    local abilityID = button.skillLineAbilityID;
+                    local equipmentSlot = rune_by_id[abilityID].equipmentSlot;
+                    button:SetScript("OnClick", function(_, mouseButton)
+                        RuneButtonOnClick(mouseButton, abilityID, equipmentSlot);
+                    end);
+                    button.name:Hide();
+                    button.typeName:Hide();
+                    button:SetWidth(RUNE_BUTTON_HEIGHT);
+                    local prevButton = buttons[currentButton - 1];
+                    button:ClearAllPoints();
+                    if runeIndex % 4 == 1 then
+                        button:SetPoint("TOPLEFT", buttons[prevRowStart], "BOTTOMLEFT");
+                        prevRowStart = currentButton;
+                    else
+                        button:SetPoint("LEFT", prevButton, "RIGHT");
+                    end
+                    button:Show();
+					currentButton = currentButton + 1;
+				end
+			end
         end
     end
-
-    local buttons = EngravingFrame.scrollFrame.buttons
-    -- workaround for button 13 never being hidden but causing issues
-    if C_Engraving.IsEquippedFilterEnabled() then
-        local button = buttons[13]
-        if button then
-            button:Hide()
-        end
+    while currentButton <= #buttons do
+        local button = buttons[currentButton];
+        button:Hide();
+        currentButton = currentButton + 1;
     end
+    local totalHeight = math.floor(numRunes / 4) * RUNE_BUTTON_HEIGHT;
+    totalHeight = totalHeight + (numHeaders * RUNE_HEADER_BUTTON_HEIGHT);
+    HybridScrollFrame_Update(scrollFrame, totalHeight+10, 348);
+end
 
-    for _, button in ipairs(buttons) do
-        if button and button:IsShown() then
-            local abilityID = button.skillLineAbilityID
-            local equipmentSlot = rune_by_id[button.skillLineAbilityID].equipmentSlot
-            button:SetScript("OnClick", function(_, mouseButton)
-                RuneButtonOnClick(mouseButton, abilityID, equipmentSlot)
-            end)
-        end
+local function AddMoreButtons()
+    local scrollFrame = EngravingFrame.scrollFrame;
+    local buttons = scrollFrame.buttons;
+    local parentName = scrollFrame:GetName();
+    local buttonName = parentName and (parentName .. "Button") or nil;
+    local point = "TOP";
+    local relativePoint = "BOTTOM";
+
+    for i = #buttons, 15 do
+        local button = CreateFrame("BUTTON", buttonName and (buttonName..1) or nil, scrollFrame.scrollChild, "RuneSpellButtonTemplate");
+        tinsert(buttons, button);
     end
 end
 
 local function EventHandler(self, event, addonName)
     if event == "ADDON_LOADED" and addonName == "Blizzard_EngravingUI" then
-        hooksecurefunc("EngravingFrame_UpdateRuneList", UpdateButtons)
+        AddMoreButtons()
+        hooksecurefunc("EngravingFrame_UpdateRuneList", UpdateButtons);
     end
 end
 
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("ADDON_LOADED")
-frame:SetScript("OnEvent", EventHandler)
+local frame = CreateFrame("Frame");
+frame:RegisterEvent("ADDON_LOADED");
+frame:SetScript("OnEvent", EventHandler);
 
 
